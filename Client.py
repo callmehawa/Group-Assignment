c7ea0f34c86e233ead88bb87605b640967ae8e8e
callmehawa <thatal.pawan@gmail.com>
2023-09-28 00:16:22 +0100

    Adding serialisation and printing it in json format.

--- Client.py
+++ Client.py
@@ -3,11 +3,16 @@
 """Creating a client for a simple client server network which will serialise and ncrypt a string 
 before sending it to the server"""
 
+import json
+
 # Create an empty dictionary
 my_dict = {}
 
 # Create a dictionary with key-value pairs
 dictionary = {"name": "Pawan", "age": 40, "city": "Preston"}
 
-#print dictionary
-print(dictionary)
+#serialse my_dict to json format
+
+json_data = json.dumps(dictionary)
+print(json_data)
+
