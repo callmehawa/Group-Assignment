7d76d5e4d8cc97c9a205db5a6186b1c5a4d824d3
callmehawa <thatal.pawan@gmail.com>
2023-09-27 23:54:30 +0100

    Added a simple dictionary with some doc string This is the first step to writting a Client server code

---
+++ Client.py
@@ -0,0 +1,13 @@
+
+#Client sevrer Network
+"""Creating a client for a simple client server network which will serialise and ncrypt a string 
+before sending it to the server"""
+
+# Create an empty dictionary
+my_dict = {}
+
+# Create a dictionary with key-value pairs
+dictionary = {"name": "Pawan", "age": 40, "city": "Preston"}
+
+#print dictionary
+print(dictionary)
