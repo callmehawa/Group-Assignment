d152e604de0d24c7bb3b35e71c61859bf965c8e9
callmehawa <thatal.pawan@gmail.com>
2023-10-03 13:40:52 +0100

    trying different methods of unit testing

--- client_unittest.py
+++ client_unittest.py
@@ -1,25 +1,14 @@
 import unittest
-from . import Client.py  # Import the code you want to test
+from unittest.mock import patch, MagicMock
+import Client  # Import the code you want to test
 
 class TestClient(unittest.TestCase):
 
-    def test_write_and_load_key(self):
-        # Test writing and loading the key
-        Client.write_key()
-        loaded_key = Client.load_key()
-        self.assertIsNotNone(loaded_key)
-        self.assertIsInstance(loaded_key, bytes)
-
-    def test_encrypt_decrypt(self):
-        # Test encryption and decryption
-        key = Client.load_key()
-        original_data = b"Test data to encrypt and decrypt"
-        encrypted_data = Client.encrypt(original_data, key)
-        self.assertIsNotNone(encrypted_data)
-        self.assertNotEqual(encrypted_data, original_data)
-
-        decrypted_data = Client.decrypt(encrypted_data, key)
-        self.assertEqual(decrypted_data, original_data)
+    @patch('Client.send_to_server', return_value=True)
+    def test_send_file_to_server(self, mock_send_to_server):
+        # Your test code here
+        result = Client.send_file_to_server('test_file.txt')
+        self.assertTrue(result)  # Check if the result is True, indicating a successful send
 
 if __name__ == '__main__':
     unittest.main()
