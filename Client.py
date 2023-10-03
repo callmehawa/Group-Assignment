56aa9a0594cf4a03b62243d4139ac0db466b8713
callmehawa <thatal.pawan@gmail.com>
2023-10-03 10:40:09 +0100

    amended the code to print key. Changed sever message to read Hello, dicitionary received!

--- Client.py
+++ Client.py
@@ -1,9 +1,6 @@
-
-#Client sevrer Network
-"""Creating a client for a simple client server network which will serialise and ncrypt a string 
-before sending it to the server"""
-
 import socket
+import pickle
+from cryptography.fernet import Fernet
 
 # Create a socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,35 +11,72 @@ server_address = ('localhost', 12345)
 # Connect to the server
 client_socket.connect(server_address)
 
+def write_key():
+    """
+    Generate a cryptographic key and save it to a file.
+
+    This function generates a Fernet key and saves it to a file named 'key.key' in binary format.
+    """
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+def load_key():
+    """
+    Load the cryptographic key from the 'key.key' file.
+
+    Returns:
+        bytes: The Fernet key loaded from the 'key.key' file.
+    """
+    return open("key.key", "rb").read()
+
+# Generate and write a new key
+write_key()
+
+# Load the previously generated key
+key = load_key()
+print(f"Fernet Key: {key.hex()}")
+
+# Call the print_key function to print the key
+load_key()
+
 try:
-    # Send data to the server
-    message = "Hello, server!"
-    client_socket.send(message.encode('utf-8'))
+    # Add a new dictionary
+    dictionary = {"name": "Pawan", "age": 40, "city": "Preston"}
+
+    # Create a file to serialize into pickle
+    with open('dict.pickle', 'wb') as file:
+        pickle.dump(dictionary, file)
+
+    # Read the serialized data from the file
+    with open('dict.pickle', 'rb') as file:
+        serialised_data = file.read()
+
+    def encrypt(data, key):
+        """
+        Encrypt data using a Fernet key.
+
+        Args:
+            data (bytes): The data to be encrypted.
+            key (bytes): The Fernet key used for encryption.
+
+        Returns:
+            bytes: The encrypted data.
+        """
+        f = Fernet(key)
+        encrypted_data = f.encrypt(data)
+        return encrypted_data
+
+    # Encrypt the serialized data
+    encrypted_data = encrypt(serialised_data, key)
+
+    # Send encrypted data to the server
+    client_socket.send(encrypted_data)
 
     # Receive a response from the server
     response = client_socket.recv(1024)
-    print("Received response from server: {}".format(response.decode('utf-8')))
+    print(f"Message from server: {response.decode('utf-8')}")
+
 finally:
     # Clean up the connection
     client_socket.close()
-
-
-
-import json
-import pickle
-
-# Create an empty dictionary
-my_dict = {}
-
-# Create a dictionary with key-value pairs
-dictionary = {"name": "Pawan", "age": 40, "city": "Preston"}
-
-#serialse my_dict to json format
-
-#json_data = json.dumps(dictionary)
-#print(json_data)
-
-# Serialize the dictionary using pickle
-with open("serialized_dict.pickle", "wb") as pickle_file:
-    pickle.dump(dictionary, pickle_file)
-    print(dictionary)
