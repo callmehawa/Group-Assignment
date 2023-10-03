46bda4bed622c8586e4728c35f91c94f15632ac8
callmehawa <thatal.pawan@gmail.com>
2023-10-03 13:24:15 +0100

    amended to import client module

--- client_unittest.py
+++ client_unittest.py
@@ -1,24 +1,24 @@
 import unittest
-import Client  # Import the code you want to test
+from . import Client.py  # Import the code you want to test
 
 class TestClient(unittest.TestCase):
 
     def test_write_and_load_key(self):
         # Test writing and loading the key
-        client.write_key()
-        loaded_key = client.load_key()
+        Client.write_key()
+        loaded_key = Client.load_key()
         self.assertIsNotNone(loaded_key)
         self.assertIsInstance(loaded_key, bytes)
 
     def test_encrypt_decrypt(self):
         # Test encryption and decryption
-        key = client.load_key()
+        key = Client.load_key()
         original_data = b"Test data to encrypt and decrypt"
-        encrypted_data = client.encrypt(original_data, key)
+        encrypted_data = Client.encrypt(original_data, key)
         self.assertIsNotNone(encrypted_data)
         self.assertNotEqual(encrypted_data, original_data)
 
-        decrypted_data = client.decrypt(encrypted_data, key)
+        decrypted_data = Client.decrypt(encrypted_data, key)
         self.assertEqual(decrypted_data, original_data)
 
 if __name__ == '__main__':
